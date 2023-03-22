c080d599e8976633bfb520365945e6578fd3ac98
Felipe Reinaux <felipereinaux@gmail.com>
2023-03-22 19:21:49 -0300

    post movies

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
+const movies = require('./data.json')
 
 app.get('/', (req, res) => {
   res.send('Star Wars API')
 })
 
 app.get('/movies', (req, res) => {
-  res.send(require('./data.json'))
+  res.send(movies)
+})
+
+app.post('/movies', (req, res) => {
+  const movie = req.body
+
+  movies.push(movie)
+
+  res.send(movies)
 })
 
 app.listen('3000', () => console.log('App listening'))
