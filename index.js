042586ec31f0d7bb426aad181b8e4a040250628d
Felipe Reinaux <felipereinaux@gmail.com>
2023-03-22 21:51:26 -0300

    post movies validations

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const { body, validationResult } = require('express-validator')
+
 const app = express()
 app.use(express.json())
+
 const movies = require('./data.json')
 
 app.get('/', (req, res) => {
@@ -11,12 +14,24 @@ app.get('/movies', (req, res) => {
   res.send(movies)
 })
 
-app.post('/movies', (req, res) => {
-  const movie = req.body
+app.post('/movies',
+  body('movie')
+    .notEmpty().withMessage('required field')
+    .isString().withMessage('must be a string value'),
+  body('year')
+    .notEmpty().withMessage('required field')
+    .isInt().withMessage('must be an int value')
+    .isLength({ min: 4, max: 4 }).withMessage('must be 4 digit length'),
+  (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).send({ errors: errors.array() })
+    }
+    const movie = req.body
 
-  movies.push(movie)
+    movies.push(movie)
 
-  res.send(movies)
-})
+    res.send(movies)
+  })
 
 app.listen('3000', () => console.log('App listening'))
